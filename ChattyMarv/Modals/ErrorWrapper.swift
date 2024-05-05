c54fd1d8d4df951f0d29c6d170e7e3fff146a6e4
Daniel Platt <github@ofdan.co.uk>
2024-05-05 17:46:26 +0100

    #1 Add missing files

---
+++ ChattyMarv/Modals/ErrorWrapper.swift
@@ -0,0 +1,17 @@
+/*
+ See Apple Sample LICENSE for this sample’s licensing information.
+ */
+
+import Foundation
+
+struct ErrorWrapper: Identifiable {
+    let id: UUID
+    let error: Error
+    let guidance: String
+
+    init(id: UUID = UUID(), error: Error, guidance: String) {
+        self.id = id
+        self.error = error
+        self.guidance = guidance
+    }
+}
